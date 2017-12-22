6c3f327ca81495fcbe98be2506785dbba6b09013
MisLink <gjq.uoiai@outlook.com>
2017-12-22 20:00:19 +0800

    Temporary commit.
    Signed-off-by: MisLink <gjq.uoiai@outlook.com>

--- preface/smallest_free_id/improvement_2/min_free.cpp
+++ preface/smallest_free_id/improvement_2/min_free.cpp
@@ -4,18 +4,17 @@
 #include "../generate_data.cpp"
 using namespace std;
 
-int min_free(vector<int>& lst, int l, int u) {
+int min_free(vector<int>::iterator begin, vector<int>::iterator end, int l,
+             int u) {
   int m = (l + u) / 2;
-  if (lst.empty()) {
+  if (begin == end) {
     return l;
   }
-  auto it = partition(lst.begin(), lst.end(), [m](int i) { return i <= m; });
-  auto left = vector<int>(lst.begin(), it);
-  auto right = vector<int>(it, lst.end());
-  if (left.size() == m - l + 1) {
-    return min_free(right, m + 1, u);
+  auto it = partition(begin, end, [m](int i) { return i <= m; });
+  if (it - begin == m - l + 1) {
+    return min_free(it, end, m + 1, u);
   } else {
-    return min_free(left, l, m);
+    return min_free(begin, it, l, m);
   }
 }
 
@@ -23,21 +22,26 @@ int min_free_(vector<int> lst) {
   int l = 0;
   int n = lst.size();
   int u = n - 1;
-  while (n > 0) {
+  auto begin = lst.begin();
+  auto end = lst.end();
+  while (end - begin > 0) {
     int m = (l + u) / 2;
     int left = 0;
-    for (int right = 0; right < n; right++) {
-      if (lst[right] <= m) {
-        swap(lst[left], lst[right]);
+    for (auto right = begin; right < end; right++) {
+      if (*right <= m) {
+        auto temp = *right;
+        *right = *(begin + left);
+        *(begin + left) = temp;
+        // swap(*(begin + left), *right);
         left++;
       }
     }
     if (left == m - l + 1) {
-      lst.erase(lst.begin(), lst.begin() + left);  // complexity: linear
-      n = n - left;
+      // lst.erase(lst.begin(), lst.begin() + left);  // complexity: linear
+      begin = end - left;
       l = m + 1;
     } else {
-      n = left;
+      end = begin + left;
       u = m;
     }
   }
@@ -46,14 +50,15 @@ int min_free_(vector<int> lst) {
 
 int main() {
   int hundred_thousand = 100000;
-  int million = 1000000;
+  // int million = 1000000;
+  int million = 100;
   clock_t total = 0;
-  size_t loop_times = 10;
+  size_t loop_times = 1;
   for (size_t i = 0; i < loop_times; i++) {
     auto lst = generate_data(million, loop_times);
     clock_t start_time = clock();
-    int v1 = min_free(lst, 0, lst.size() - 1);
-    cout << "result 1: " << v1 << endl;
+    // int v1 = min_free(lst.begin(), lst.end(), 0, lst.size() - 1);
+    // cout << "result 1: " << v1 << endl;
     int v2 = min_free_(lst);
     cout << "result 2: " << v2 << endl;
     total += clock() - start_time;
