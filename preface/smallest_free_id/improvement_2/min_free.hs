b1fa7ebe52895b967085341689c4077ecb01f8a6
MisLink <gjq.uoiai@outlook.com>
2018-02-12 19:58:55 +0800

    Format haskell code.
    Signed-off-by: MisLink <gjq.uoiai@outlook.com>

--- preface/smallest_free_id/improvement_2/min_free.hs
+++ preface/smallest_free_id/improvement_2/min_free.hs
@@ -1,11 +1,14 @@
+module MinFree where
 import Data.List
 
-bsearch xs l u
-  | null xs = l
-  | length as == m - l + 1 = bsearch bs (m + 1) u
-  | otherwise = bsearch as l m
-  where
-    m = (l + u) `div` 2
-    (as, bs) = partition (<= m) xs
+bsearch :: [Int] -> Int -> Int -> Int
+bsearch xs l u | null xs                = l
+               | length as == m - l + 1 = bsearch bs (m + 1) u
+               | otherwise              = bsearch as l m
+ where
+  m        = (l + u) `div` 2
+  (as, bs) = partition (<= m) xs
 
+
+minFree :: [Int] -> Int
 minFree xs = bsearch xs 0 (length xs - 1)
